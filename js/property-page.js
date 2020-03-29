de7c118e3197c92f04612fb7744c0bb12092f426
id-java <id-java@users.noreply.github.com>
2020-03-29 16:56:30 -0400

    updated cred and color

--- js/property-page.js
+++ js/property-page.js
@@ -1,4 +1,4 @@
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
+// const track = document.querySelector('.carousel__track');
+// const slides = Array.from(track.children);
 
-console.log(track);
+// console.log(track);
